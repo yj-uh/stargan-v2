985340a00d9fccfd22fc50f487e2e1613c43dd6d
yj-uh <yj.uh@yonsei.ac.kr>
2023-02-09 16:38:47 +0900

    fork test

---
+++ train.sh
@@ -0,0 +1,5 @@
+python main.py --mode train --num_domains 3 --w_hpf 0 \
+               --lambda_reg 1 --lambda_sty 1 --lambda_ds 2 --lambda_cyc 1 \
+               --train_img_dir data/afhq_hdd/train \
+               --val_img_dir data/afhq_hdd/val
+
